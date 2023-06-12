9210008e2e23e68b922273da3debf1f6b9fab7ba
mijinB <bmj13465@gmail.com>
2023-06-12 16:52:51 +0900

    textField_Controller

--- lib/features/authentication/username_screen.dart
+++ lib/features/authentication/username_screen.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone_study/constants/gaps.dart';
 import 'package:tiktok_clone_study/constants/sizes.dart';
 
-class UsernameScreen extends StatelessWidget {
+class UsernameScreen extends StatefulWidget {
   const UsernameScreen({super.key});
+
+  @override
+  State<UsernameScreen> createState() => _UsernameScreenState();
+}
+
+class _UsernameScreenState extends State<UsernameScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+
+  String _username = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,27 +33,74 @@ class UsernameScreen extends StatelessWidget {
           'Sign up',
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.symmetric(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
           horizontal: Sizes.size32,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Gaps.v36,
-            Text(
+            const Text(
               'Create username',
               style: TextStyle(
                 fontSize: Sizes.size20 + Sizes.size2,
                 fontWeight: FontWeight.w800,
               ),
             ),
-            Gaps.v14,
-            Text(
+            Gaps.v12,
+            const Text(
               'You can always change this later.',
               style: TextStyle(
                 color: Colors.grey,
                 fontSize: Sizes.size14 + Sizes.size1,
+              ),
+            ),
+            Gaps.v28,
+            TextField(
+              controller: _usernameController,
+              cursorColor: Theme.of(context).primaryColor,
+              decoration: const InputDecoration(
+                hintText: 'Username',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ),
+            Gaps.v28,
+            FractionallySizedBox(
+              widthFactor: 1.0,
+              child: AnimatedContainer(
+                duration: const Duration(
+                  milliseconds: 300,
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size14,
+                ),
+                decoration: BoxDecoration(
+                  color: _username.isEmpty
+                      ? Colors.grey.shade300
+                      : Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(
+                    Sizes.size5,
+                  ),
+                ),
+                child: Text(
+                  'Next',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: _username.isEmpty ? Colors.grey : Colors.white,
+                    fontSize: Sizes.size16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
             ),
           ],
